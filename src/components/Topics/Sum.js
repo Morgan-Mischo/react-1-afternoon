76b276c66ad0333c6b78fe409381541a498ee18f
Morgan-Mischo <goramorgan@gmail.com>
2019-06-17 16:46:41 -0600

    close to done

---
+++ src/components/Topics/Sum.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'; 
+
+export default class Sum extends Component {
+    render () {
+        return (
+            <div className ="puzzleBox sumPB">
+                <h4>Sum</h4>
+            </div>
+        )
+    }
+}
